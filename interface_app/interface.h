a06c00a76783b810554c493a5196a16e49b69e57
zhengxuntai <302780150@qq.com>
2017-12-27 19:29:32 +0800

    硬件串口任务
    Signed-off-by: zhengxuntai <302780150@qq.com>

---
+++ interface_app/interface.h
@@ -0,0 +1,29 @@
+#ifndef			__INTERFACE_H__
+#define			__INTERFACE_H__
+
+#define			INTERFACE_DATA_MAX_LEN					128
+
+
+typedef	enum 
+{
+	INTERFACE_UART,
+	INTERFACE_SPI,
+}INTERFACE_TYPE;
+
+
+
+#define			INTERFACE_EVT_RX			Event_Id_00
+#define			INTERFACE_EVT_TX			Event_Id_01
+#define			INTERFACE_EVT_ALL			0xffff
+
+
+
+
+void InterfaceTaskCreate(void);
+
+void InterfaceSend(uint8_t * datap, uint8_t len);
+
+
+
+
+#endif			// __INTERFACE_H__
