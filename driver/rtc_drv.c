3dd43820f85680187dedaa690a248fe734736b45
zhengxuntai <302780150@qq.com>
2017-12-27 19:28:27 +0800

    增加rtc功能
    Signed-off-by: zhengxuntai <302780150@qq.com>

---
+++ driver/rtc_drv.c
@@ -0,0 +1,203 @@
+#include "../general.h"
+
+
+// **************************************************************************
+// variable
+// rtc timer
+static Clock_Struct rtcSecondsClock;     /* not static so you can see in ROV */
+
+static Clock_Handle rtcSecondsClockHandle;
+
+
+// semaphore 
+static Semaphore_Struct rtcSemStruct;
+static Semaphore_Handle rtcSemHandle;
+
+
+// rtc
+static Calendar rtc;
+
+static void (*RtcSecIsbCB)(void);
+
+// *******************************************************************************
+// *Funtion name: MonthMaxDay
+// *Description :
+// *
+// *Input: the month and year
+// *Output:the last day of the month
+// /******************************************************************************
+uint8_t MonthMaxDay(uint16_t year_1, uint8_t month_1)
+{
+    uint8_t maxday;
+
+    // leap year judgement
+    if((year_1%4) == 0)
+    {
+        if((year_1 % 100) != 0)
+            maxday = 29;
+        else
+        {
+            if((year_1%400) == 0)
+                maxday  = 29;
+            else
+                maxday  = 28;
+        }
+    }
+    else
+    {
+        maxday = 28;
+    }
+    
+    switch(month_1)
+    {
+        case 1:
+            return(31);
+        case 2:
+            return(maxday);
+        case 3:
+            return(31);
+        case 4:
+            return (30);
+        case 5:
+            return(31);
+        case 6:
+            return(30);
+        case 7:
+            return(31);
+        case 8:
+            return (31);
+        case 9:
+            return(30);
+        case 10:
+            return(31);
+        case 11:
+            return(30);
+        case 12:
+            return (31);
+        default:
+            break;
+    }
+        return 0xff;
+}
+
+
+void RtcSecondsIsrCb(UArg arg0)
+{
+    // Semaphore_pend(rtcSemHandle, BIOS_WAIT_FOREVER);
+
+    rtc.sec++;
+    if(rtc.sec >= 60){
+        rtc.sec = 0;
+        rtc.min++;
+        if(rtc.min >= 60){
+            rtc.min = 0;
+            rtc.hour++;
+            if(rtc.hour >= 24){
+                rtc.hour = 0;
+                rtc.day++;
+                rtc.week++;
+                if(rtc.week > 6)
+                    rtc.week = 0;
+
+                if(rtc.day > MonthMaxDay(rtc.year, rtc.month)){
+                    rtc.day = 1;
+                    rtc.month++;
+                    if(rtc.month >12){
+                        rtc.month = 1;
+                        rtc.year ++;
+                    }
+                }
+            }
+        }
+    }
+    // Semaphore_post(rtcSemHandle);
+
+    RtcSecIsbCB();
+}
+
+
+
+
+//***********************************************************************************
+//
+// RTC init Calendar Mode.
+//
+//***********************************************************************************
+void RtcInit(void (*Cb)(void))
+{
+
+    /* Create clock object which is used for fast report timeout */
+    Clock_Params clkParams;
+    clkParams.period = 1000;
+    clkParams.startFlag = FALSE;
+    Clock_construct(&rtcSecondsClock, RtcSecondsIsrCb, 1, &clkParams);
+    rtcSecondsClockHandle = Clock_handle(&rtcSecondsClock);
+
+
+    /* Construct the semparams for interface  */
+    Semaphore_Params semParams;
+    Semaphore_Params_init(&semParams);
+    semParams.mode = Semaphore_Mode_BINARY;
+    Semaphore_construct(&rtcSemStruct, 1, &semParams);
+    rtcSemHandle = Semaphore_handle(&rtcSemStruct);
+
+
+
+    rtc.year  = 2017;
+    rtc.month = 12;
+    rtc.day   = 30;
+    rtc.week  = 6;
+    rtc.hour  = 0;
+    rtc.min   = 0;
+    rtc.sec   = 0;
+
+
+    RtcSecIsbCB = Cb;
+}
+
+//***********************************************************************************
+//
+// RTC start.
+//
+//***********************************************************************************
+void RtcStart(void)
+{
+    if(Clock_isActive(rtcSecondsClockHandle) == false)
+        Clock_start(rtcSecondsClockHandle);
+}
+
+//***********************************************************************************
+//
+// RTC stop.
+//
+//***********************************************************************************
+void RtcStop(void)
+{
+    if(Clock_isActive(rtcSecondsClockHandle))
+        Clock_stop(rtcSecondsClockHandle);
+}
+
+
+
+
+//***********************************************************************************
+//
+// RTC set Calendar time.
+//
+//***********************************************************************************
+void Rtc_set_calendar(Calendar *currentTime)
+{
+    Semaphore_pend(rtcSemHandle, BIOS_WAIT_FOREVER);
+    memcpy(&rtc, currentTime, sizeof(Calendar));
+    Semaphore_post(rtcSemHandle);
+}
+
+//***********************************************************************************
+//
+// RTC get Calendar time.
+//
+//***********************************************************************************
+Calendar Rtc_get_calendar(void)
+{
+    return rtc;
+}
---
+++ driver/rtc_drv.h
@@ -0,0 +1,36 @@
+#ifndef			__RTC_DRV_H__
+#define			__RTC_DRV_H__
+
+
+typedef struct Calendar
+{
+    //! Seconds of minute between 0-59
+    uint8_t sec;
+    //! Minutes of hour between 0-59
+    uint8_t min;
+    //! Hour of day between 0-23
+    uint8_t hour;
+    //! Day of week between 0-6
+    uint8_t week;
+    //! Day of month between 1-31
+    uint8_t day;
+    //! Month between 0-11
+    uint8_t month;
+    //! Year between 0-4095
+    uint16_t year;
+} Calendar;
+
+
+void RtcInit(void (*Cb)(void));
+
+void RtcStart(void);
+
+void RtcStop(void);
+
+void Rtc_set_calendar(Calendar *currentTime);
+
+Calendar Rtc_get_calendar(void);
+
+#endif    // __RTC_DRV_H__
+
+
