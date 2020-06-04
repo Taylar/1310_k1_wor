df0381105fa194c771fe1aec053a0a01478c11d2
zhengxuntai <302780150@qq.com>
2020-06-04 17:11:49 +0800

    增加声音驱动
    Signed-off-by: zhengxuntai <302780150@qq.com>

---
+++ electricshock/sound_driver.c
@@ -0,0 +1,61 @@
+#include "../general.h"
+
+
+#define SOUND_DATA_PIN                                IOID_25     //声音数据线
+
+#define SOUND_BUSY_PIN                             	  IOID_24      //声音状态线
+
+#define SOUND_STATE_BUSY							1
+#define SOUND_STATE_IDLE							0
+
+
+static PIN_State   soundDriverState;
+static PIN_Handle  soundDriverHandle;
+
+const PIN_Config soundPinTable[] = {
+    SOUND_DATA_PIN | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,       /* 低档电压档位         */
+    SOUND_BUSY_PIN | PIN_INPUT_EN | PIN_PULLDOWN,
+    PIN_TERMINATE
+};
+
+void SoundDriverInit(void)
+{
+    soundDriverHandle = PIN_open(&soundDriverState, soundPinTable);
+}
+
+
+
+void SoundDriverSet(uint8_t soundType)
+{
+	uint8_t i;
+	uint8_t delayTime = 0;
+	while(delayTime++<20){
+		if(PIN_getInputValue(SOUND_BUSY_PIN) == SOUND_STATE_IDLE)
+			break;
+	}
+	if(delayTime >= 20)
+		return;
+
+	IntMasterDisable();
+	PIN_setOutputValue(soundDriverHandle, SOUND_DATA_PIN, 1);
+	delay_ms(8);
+	PIN_setOutputValue(soundDriverHandle, SOUND_DATA_PIN, 0);
+	delay_ms(1);
+
+	for(i = 0; i < 8; i++){
+		if(soundType & (0x80>>i)){
+			PIN_setOutputValue(soundDriverHandle, SOUND_DATA_PIN, 1);
+			delay_us(500);
+			PIN_setOutputValue(soundDriverHandle, SOUND_DATA_PIN, 0);
+			delay_us(1500);
+		}else{
+			PIN_setOutputValue(soundDriverHandle, SOUND_DATA_PIN, 1);
+			delay_us(1500);
+			PIN_setOutputValue(soundDriverHandle, SOUND_DATA_PIN, 0);
+			delay_us(500);
+		}
+	}
+
+	IntMasterEnable();
+	
+}
---
+++ electricshock/sound_driver.h
@@ -0,0 +1,11 @@
+#ifndef SOUND_DRIVER_H
+#define SOUND_DRIVER_H
+
+#define  	SOUND_TYPE_			0X01
+
+
+void SoundDriverInit(void);
+void SoundDriverSet(uint8_t soundType);
+
+
+#endif //_SOUND_DRIVER_H
